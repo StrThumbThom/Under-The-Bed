6a4fb224fa5a80fffc7c14857986d6c3619a9e5a
TerraBarajas <tristan.barajas@live.com>
2017-09-09 21:41:31 -0600

    Movement working!
    Got movement working. Try it with a controller!

---
+++ Under The Bed/Assets/Demoscripts/cameratrack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameratrack : MonoBehaviour {
+
+     public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 truetarg = new Vector3(target.position.x, target.position.y, target.position.z);
+        transform.LookAt(truetarg);
+	}
+}
---
+++ Under The Bed/Assets/Demoscripts/move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    public CharacterController test;
+    public float speed;
+    public Transform currentcam;
+    Vector3 player;
+    Vector3 cam;
+    Vector3 orth;
+    Vector3 toward;
+    Vector3 horz;
+    Vector3 vert;
+    Vector3 temp;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        player = transform.position;
+        cam = currentcam.position;
+        toward = player - cam;
+        toward.y = transform.position.y;
+        toward = toward.normalized;
+        orth = Vector3.Cross(toward, Vector3.up).normalized;
+
+        horz = -orth * (Input.GetAxis("Horizontal"));
+        vert = toward * (Input.GetAxis("Vertical"));
+        temp = horz + vert;
+        temp = temp.normalized * speed;
+        temp.y = 0;
+        test.Move(temp);
+    
+	}
+}
