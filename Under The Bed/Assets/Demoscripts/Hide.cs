3759a34e6a34765b60d3f5248829d6559c2a8482
TerraBarajas <tristan.barajas@live.com>
2017-09-09 22:25:17 -0600

    Hiding complete
    Press 'e' to hide under the bed!
    (or A on an xbox controller)

---
+++ Under The Bed/Assets/Demoscripts/Hide.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour
+{
+
+    public Transform spot;
+    public GameObject a;
+    bool hidden = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((Input.GetButtonUp("Fire1")) && (Vector3.Distance(transform.position, spot.position) < 5.0))
+        {
+            if(GetComponent<Hide>().hidden == false)
+            {
+                GetComponent<Renderer>().enabled = false;
+                GetComponent<move>().enabled = false;
+                hidden = true;
+
+            }
+
+            else
+            {
+                GetComponent<Renderer>().enabled = true;
+                GetComponent<move>().enabled = true;
+                hidden = false;
+            }
+        }
+
+    }
+}
