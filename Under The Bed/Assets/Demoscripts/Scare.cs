18bd09c16b22341bc23fe96d9f0402f0aca9d81b
evibuilder <mrswimman563@gmail.com>
2017-09-12 00:34:30 -0600

    Meter added to canvas and funcntionality to scare and kid
    A meter has been added to the canvas and one can now scare the kid out
    by pressing j or k.   One won't work until the other has been used.
    Also, kid can now leave the room and come back.

---
+++ Under The Bed/Assets/Demoscripts/Scare.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scare : MonoBehaviour {
+
+	public Transform spot;
+	public kid c;
+	bool scareFlag1 = true;
+	bool scareFlag2 = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKeyDown(KeyCode.J)) && (Vector3.Distance(transform.position, spot.position) < 8.0) && scareFlag1 == true && c.getAwake() == false)
+		{
+
+			scareFlag1 = false;
+			scareFlag2 = true;
+			c.setAwake ();
+
+		}
+		if ((Input.GetKeyDown(KeyCode.K)) && (Vector3.Distance(transform.position, spot.position) < 8.0) && scareFlag2 == true && c.getAwake() == false)
+		{
+
+			scareFlag1 = true;
+			scareFlag2 = false;
+			c.setAwake ();
+		}
+
+	}
+}
---
+++ Under The Bed/Assets/Demoscripts/kid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kid : MonoBehaviour {
+	float t;
+	bool isAwake;
+	bool isGoingBack;
+	public Transform start;
+	public Transform end;
+	float time;
+	// Use this for initialization
+	void Start () {
+		isAwake = false;
+		isGoingBack = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAwake == true && isGoingBack == false) {
+			t += Time.deltaTime / 5;
+			transform.position = Vector3.Lerp (start.position, end.position, t);
+		}
+		if (transform.position == end.position) {
+			isGoingBack = true;
+
+			t = 0;
+			//t += Time.deltaTime / 12;
+			//transform.position = Vector3.Lerp (end.position, start.position, t);
+		}
+		if (isAwake == true && isGoingBack == true) {
+			t += Time.deltaTime / 8;
+			transform.position = Vector3.Lerp (end.position, start.position, t);
+		}
+		if (transform.position == start.position) {
+			isAwake = false;
+			t = 0;
+			isGoingBack = false;
+			Vector3 temp = transform.rotation.eulerAngles;
+			temp.z = -90.0f;
+		}
+
+	}
+	public void setAwake ()
+	{
+		isAwake = true;
+	}
+	public bool getAwake()
+	{
+		return isAwake;
+	}
+}
