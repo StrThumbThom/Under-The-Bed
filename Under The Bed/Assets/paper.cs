a4a04b3fc4472235400a8da936d070dd60f4f07d
TerraBarajas <tristan.barajas@live.com>
2017-09-20 18:22:35 -0600

    Paper dissapears now
    Kinda mesy code, but it works

--- Under The Bed/Assets/paper.cs
+++ Under The Bed/Assets/paper.cs
@@ -9,9 +9,11 @@ public class paper : MonoBehaviour {
     public Text text2;
     public Scare mon;
     public Image banan;
-	
+    public GameObject prefab;
+
+
     // Use this for initialization
-	void Start () {
+    void Start () {
         GetComponent<Renderer>().material.color = Color.green;
         banan.enabled = false;
         text.enabled = false;
@@ -37,5 +39,6 @@ public class paper : MonoBehaviour {
         banan.enabled = false;
         text.enabled = false;
         text2.enabled = false;
+        prefab.SetActive(false);
     }
 }
